83d86bffeb141636a4c6c8a62539c6c72192b326
Matt <matthew.watts.mw@gmail.com>
2018-04-07 18:53:06 -0500

    client part requests

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super(){}
+
+    this.order = this.order.bind(this);
+    this.fulfill = this.order.bind(this);
+    this.ship = this.order.bind(this);
+  }
+
+  order(){
+    socket.emit('order',
+    { 'station': 5,
+      'part: 
+        {
+          'partNumber': '87647',
+          'ledColor': 'red',
+        }  
+    });
+  }
+
+  fulfill(){
+    socket.emit('fulfillment',
+    { 'station': 5,
+      'part: 
+        {
+          'partNumber': '5647',
+          'ledColor': 'blue',
+        }  
+    });
+  }
+
+  ship(){
+    socket.emit('shipment',
+    { 'station': 5,
+      'part: 
+        {
+          'partNumber': '34547',
+          'ledColor': 'yellow',
+        }  
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>My React App!</h1>
+       <button onClick={}>Bin Leaves Station</button>
+       <button onClick={}>Bin Enters Delivery Cart</button>
+       <button onClick={}>Bin Enters Station</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,11 @@ import App from './components/App';
 import openSocket from 'socket.io-client';
 
 const socket = openSocket('http://localhost:8080');
-socket.emit('station_update',
-[
-  {
-    'partNumber': '5647',
-    'ledColor': 'red',
-  },
-  {
-    'partNumber': '52797',
-    'ledColor': 'blue',
-  }
-]);
+
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
