6809f43c0655825f34292f62ca35b08570dfbd28
Larson <larsonma@ad.msoe.edu>
2018-04-07 18:14:38 -0500

    New parts now added to mongoose

--- server/SocketIO/index.js
+++ server/SocketIO/index.js
@@ -1,23 +1,21 @@
 const Part = require('../Models/Part.js').Part;
 
+const createPart = function(part){
+    let p = new Part({
+        partNumber: part.partNumber,
+        ledColor: part.ledColor
+    });
+
+    p.save((err, part) => {
+        if (err) return console.error(err);
+    });
+}
+
 const socketListeners = io => {
     io.on('connection', socket => {
         socket.on('station_update', data => {
-            
-            let parts = data.map( part => new Part({
-                        partNumber:part.partNumber,
-                        ledColor:part.ledColor
-                    }
-                );
-
-                part.save((err, part) => {
-                    if (err) return console.error(err);                    
-                });
-            )}
-        );
-
+            data.map(part => createPart(part))
         });
-
     });
 }
 
