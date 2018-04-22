0f881f104311b9d41acb9183805041c9291a56f6
Larson <larsonma@ad.msoe.edu>
2018-04-22 09:55:46 -0500

    Added Part object

--- server/Models/Part.js
+++ server/Models/Part.js
@@ -5,6 +5,47 @@ var partSchema = mongoose.Schema({
     ledColor: String
  });
 
-var Part = mongoose.model('Part', partSchema);
+const states = {
+    ordered, transit1, transit2, fulfilled, unknown 
+}
 
-module.exports.Part = Part;
+class Part {
+    constructor(number, id){
+        this.number = number;
+        this.id = id;
+        this.state = states.fulfilled;
+    }
+
+    changeState(){
+        this.state = this._next();
+    }
+
+    _next(){
+        switch(this.state){
+            case states.ordered:
+                this.state = states.transit1;
+                break;
+            case states.transit1:
+                this.state = states.transit2;
+                break;
+            case states.transit2:
+                this.state = states.fulfilled;
+                break;
+            case states.fulfilled:
+                this.state = states.ordered;
+                break;
+            // case default:
+            //     console.log('Part in unknown state');
+            //     this.state = states.unknown;
+            //     break;
+        }
+    }
+}
+//var Part = mongoose.model('Part', partSchema);
+
+//module.exports.Part = Part;
+
+module.exports.Part = {
+    mongoose_part: mongoose.model('Part', partSchema),
+    Part: Part
+}
